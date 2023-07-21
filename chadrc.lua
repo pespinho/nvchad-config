3623a536296033108e477638fb38973c194f7393
Pespinho <pespinho@users.noreply.github.com>
2023-07-22 00:23:23 +0200

    added custom configuration for lazy_nvim

--- chadrc.lua
+++ chadrc.lua
@@ -1,6 +1,7 @@
----@type ChadrcConfig 
- local M = {}
- M.ui = {theme = 'catppuccin'}
- M.plugins = 'custom.plugins'
- M.mappings = require "custom.mappings"
+---@type ChadrcConfig
+local M = {}
+M.ui = { theme = 'catppuccin' }
+M.plugins = 'custom.plugins'
+M.mappings = require "custom.mappings"
+M.lazy_nvim = require "custom.configs.lazy_nvim"  -- config for lazy.nvim startup options
 return M
