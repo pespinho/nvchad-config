7da2675bd29c33cbff5ea82bc4b69c984dc4c132
Pespinho <pespinho@users.noreply.github.com>
2023-07-22 00:19:21 +0200

    added some options after watching a primeagen video

--- init.lua
+++ init.lua
@@ -7,3 +7,22 @@ vim.api.nvim_exec('language en_US', true)
 -- Show relative numbers as default.
 vim.opt.relativenumber = true
 vim.opt.number = true
+
+-- Primeagen Options
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.updatetime = 50
